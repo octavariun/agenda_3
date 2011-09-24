28fed7b81b572c86e37b120ee7fa5d4fef5ee2be
cassio <cassio@cassio-linux.(none)>
2011-09-24 13:57:55 -0300

    oaooa

---
+++ app/models/usuario.rb
@@ -0,0 +1,9 @@
+class Usuario < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :http_authenticatable, :token_authenticatable, :validatable, :confirmable, :lockable, :timeoutable and :activatable
+  devise :registerable, :authenticatable, :recoverable,
+         :rememberable, :trackable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
+end
