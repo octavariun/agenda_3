3c6df54f411ca2f2a829b9b2fa50ca5ce91c0006
cassio felipe de souza de costa <octavariun@gmail.com>
2013-05-10 14:27:16 -0300

    asd

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,10 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+   #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+   Bundler.require(:default, :assets, Rails.env)
 end
 
 module Agenda3
@@ -54,7 +55,9 @@ module Agenda3
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
-    # Enable the asset pipeline
+	#config.assets.initialize_on_precompile = false
+	
+	# Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
